b2ccbcdc0cd82d12ad9ccb0ba93f065459b36771
Zsolt Sebe <146239809+szsolt0@users.noreply.github.com>
2025-12-12 00:42:21 +0100

    work on tests

--- code/library/cpp/tests/test_seccomp.cpp
+++ code/library/cpp/tests/test_seccomp.cpp
@@ -47,3 +47,23 @@ TEST(SeccompBuilder, Build) {
 	auto rule = builder->build();
 	EXPECT_TRUE(rule) << "building failed";
 }
+
+TEST(SeccompBuilder, Apply) {
+	auto builder = SeccompBuilder::init();
+	EXPECT_TRUE(builder) << "init builder failed: " << builder.error();
+
+	// adding new rules should work
+	EXPECT_TRUE(builder->allow("io")) << "allow rule failed";
+
+	// adding non-existing rules should NOT work
+	EXPECT_FALSE(builder->allow("jdijdsiojiodsj")) << "bad allow rule succeed";
+
+	// building the rule should work
+	auto rule = builder->build();
+	EXPECT_TRUE(rule) << "building failed";
+
+	set_no_new_privs();
+
+	auto res = rule->view().apply();
+	EXPECT_TRUE(res) << "rule applying failed: " << res.error();
+}
