45147050b033f3972dcff7c632a45e76e88b5ca0
Zsolt Sebe <146239809+szsolt0@users.noreply.github.com>
2026-05-07 16:37:52 +0200

    add tests

--- code/library/cpp/tests/test_landlock.cpp
+++ code/library/cpp/tests/test_landlock.cpp
@@ -1,48 +1,363 @@
 #include <gtest/gtest.h>
+
 #include <mylib/landlock.h>
 #include <mylib/no_new_privs.h>
+#include <mylib/detail/common_syscall.h>
 
-#include <sys/mman.h>
+#include <cerrno>
+#include <cstdlib>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 using namespace mylib;
 
-TEST(Landlock, Basic) {
-	EXPECT_TRUE(set_no_new_privs());
+namespace {
 
-	auto lk = LandlockRuleSet::init();
-	EXPECT_TRUE(lk);
+std::filesystem::path make_test_root()
+{
+    auto root = std::filesystem::temp_directory_path()
+              / ("mylib_landlock_test_" + std::to_string(::getpid()));
 
-	EXPECT_TRUE(lk->add_rule("/etc", LandlockAccess::Read));
-	EXPECT_TRUE(lk->apply());
+    std::filesystem::remove_all(root);
+    std::filesystem::create_directories(root);
+
+    return root;
 }
 
-TEST(Landlock, Allow) {
-	mkdir("test_folder", 0777);
-	mkdir("test_folder/sub", 0777);
-	creat("test_folder/a.txt", 0666);
-	creat("test_folder/sub/a.txt", 0666);
+void write_file(std::filesystem::path const& path, std::string const& content)
+{
+    std::ofstream file(path, std::ios::binary);
+    ASSERT_TRUE(file) << "failed to create file: " << path;
+    file << content;
+}
 
-	mkdir("test_folder_secret", 0777);
-	creat("test_folder_secret/secret.txt", 0666);
+void setup_landlock_tree(std::filesystem::path const& root)
+{
+    std::filesystem::create_directories(root / "allowed" / "sub");
+    std::filesystem::create_directories(root / "secret");
 
-	EXPECT_TRUE(set_no_new_privs());
+    write_file(root / "allowed" / "a.txt", "allowed\n");
+    write_file(root / "allowed" / "sub" / "nested.txt", "nested\n");
+    write_file(root / "secret" / "secret.txt", "secret\n");
+}
 
-	auto lk = LandlockRuleSet::init();
-	EXPECT_TRUE(lk);
+[[noreturn]] void exit_if_failed(std::expected<void, int>&& result)
+{
+    if (!result) {
+        std::exit(result.error());
+    }
 
-	EXPECT_TRUE(lk->add_rule("/etc", LandlockAccess::Read));
-	EXPECT_TRUE(lk->add_rule("test_folder", LandlockAccess::ReadWrite));
-	EXPECT_TRUE(lk->apply());
+    std::exit(0);
+}
 
-	auto fd1 = detail::sys_open("/etc/passwd", O_RDONLY|O_CLOEXEC, 0);
-	EXPECT_TRUE(fd1 > 0);
+template <class T>
+T unwrap_or_exit(std::expected<T, int>&& result)
+{
+    if (!result) {
+        std::exit(result.error());
+    }
 
-	auto fd2 = detail::sys_open("test_folder/a.txt", O_RDWR|O_CLOEXEC, 0);
-	EXPECT_TRUE(fd2 > 0);
+    return std::move(result).value();
+}
 
-	auto fd3 = detail::sys_open("test_folder/sub/a.txt", O_RDWR|O_CLOEXEC, 0);
-	EXPECT_TRUE(fd3 > 0);
+void unwrap_or_exit(std::expected<void, int>&& result)
+{
+    if (!result) {
+        std::exit(result.error());
+    }
+}
 
-	auto fd4 = detail::sys_open("test_folder_secret/secret.txt", O_RDONLY|O_CLOEXEC, 0);
-	EXPECT_FALSE(fd4 > 0);
+} // namespace
+
+TEST(Landlock, InitSucceeds)
+{
+    auto nnp = set_no_new_privs();
+    ASSERT_TRUE(nnp) << "set_no_new_privs failed: " << nnp.error();
+
+    auto lk = LandlockRuleSet::init();
+
+    EXPECT_TRUE(lk) << "LandlockRuleSet::init failed: " << lk.error();
+}
+
+TEST(Landlock, AddReadableEtcRuleSucceeds)
+{
+    auto nnp = set_no_new_privs();
+    ASSERT_TRUE(nnp) << "set_no_new_privs failed: " << nnp.error();
+
+    auto lk = LandlockRuleSet::init();
+    ASSERT_TRUE(lk) << "LandlockRuleSet::init failed: " << lk.error();
+
+    auto res = lk->add_rule("/etc", LandlockAccess::Read);
+
+    EXPECT_TRUE(res) << "add_rule failed: " << res.error();
+}
+
+TEST(Landlock, AddRuleForMissingPathFails)
+{
+    auto nnp = set_no_new_privs();
+    ASSERT_TRUE(nnp) << "set_no_new_privs failed: " << nnp.error();
+
+    auto lk = LandlockRuleSet::init();
+    ASSERT_TRUE(lk) << "LandlockRuleSet::init failed: " << lk.error();
+
+    auto res = lk->add_rule("/definitely/not/a/real/path", LandlockAccess::Read);
+
+    EXPECT_FALSE(res);
+}
+
+TEST(LandlockBehavior, AllowsReadInsideAllowedDirectory)
+{
+    auto root = make_test_root();
+    setup_landlock_tree(root);
+
+    auto allowed_dir = root / "allowed";
+    auto allowed_file = root / "allowed" / "a.txt";
+
+    ASSERT_EXIT(
+        {
+            unwrap_or_exit(set_no_new_privs());
+
+            auto lk = unwrap_or_exit(LandlockRuleSet::init());
+            unwrap_or_exit(lk.add_rule(allowed_dir.c_str(), LandlockAccess::Read));
+            unwrap_or_exit(lk.apply());
+
+            int fd = detail::sys_open(
+                allowed_file.c_str(),
+                O_RDONLY | O_CLOEXEC,
+                0
+            );
+
+            if (fd < 0) {
+                std::exit(-fd);
+            }
+
+            detail::sys_close(fd);
+            std::exit(0);
+        },
+        ::testing::ExitedWithCode(0),
+        ""
+    );
+
+    std::filesystem::remove_all(root);
+}
+
+TEST(LandlockBehavior, AllowsReadInsideSubdirectory)
+{
+    auto root = make_test_root();
+    setup_landlock_tree(root);
+
+    auto allowed_dir = root / "allowed";
+    auto nested_file = root / "allowed" / "sub" / "nested.txt";
+
+    ASSERT_EXIT(
+        {
+            unwrap_or_exit(set_no_new_privs());
+
+            auto lk = unwrap_or_exit(LandlockRuleSet::init());
+            unwrap_or_exit(lk.add_rule(allowed_dir.c_str(), LandlockAccess::Read));
+            unwrap_or_exit(lk.apply());
+
+            int fd = detail::sys_open(
+                nested_file.c_str(),
+                O_RDONLY | O_CLOEXEC,
+                0
+            );
+
+            if (fd < 0) {
+                std::exit(-fd);
+            }
+
+            detail::sys_close(fd);
+            std::exit(0);
+        },
+        ::testing::ExitedWithCode(0),
+        ""
+    );
+
+    std::filesystem::remove_all(root);
+}
+
+TEST(LandlockBehavior, DeniesReadOutsideAllowedDirectory)
+{
+    auto root = make_test_root();
+    setup_landlock_tree(root);
+
+    auto allowed_dir = root / "allowed";
+    auto secret_file = root / "secret" / "secret.txt";
+
+    ASSERT_EXIT(
+        {
+            unwrap_or_exit(set_no_new_privs());
+
+            auto lk = unwrap_or_exit(LandlockRuleSet::init());
+            unwrap_or_exit(lk.add_rule(allowed_dir.c_str(), LandlockAccess::Read));
+            unwrap_or_exit(lk.apply());
+
+            int fd = detail::sys_open(
+                secret_file.c_str(),
+                O_RDONLY | O_CLOEXEC,
+                0
+            );
+
+            if (fd >= 0) {
+                detail::sys_close(fd);
+                std::exit(1);
+            }
+
+            std::exit(-fd);
+        },
+        ::testing::ExitedWithCode(EACCES),
+        ""
+    );
+
+    std::filesystem::remove_all(root);
+}
+
+TEST(LandlockBehavior, AllowsWriteWhenReadWriteIsGranted)
+{
+    auto root = make_test_root();
+    setup_landlock_tree(root);
+
+    auto allowed_dir = root / "allowed";
+    auto allowed_file = root / "allowed" / "a.txt";
+
+    ASSERT_EXIT(
+        {
+            unwrap_or_exit(set_no_new_privs());
+
+            auto lk = unwrap_or_exit(LandlockRuleSet::init());
+            unwrap_or_exit(lk.add_rule(allowed_dir.c_str(), LandlockAccess::ReadWrite));
+            unwrap_or_exit(lk.apply());
+
+            int fd = detail::sys_open(
+                allowed_file.c_str(),
+                O_WRONLY | O_CLOEXEC,
+                0
+            );
+
+            if (fd < 0) {
+                std::exit(-fd);
+            }
+
+            detail::sys_close(fd);
+            std::exit(0);
+        },
+        ::testing::ExitedWithCode(0),
+        ""
+    );
+
+    std::filesystem::remove_all(root);
+}
+
+TEST(LandlockBehavior, DeniesWriteWhenOnlyReadIsGranted)
+{
+    auto root = make_test_root();
+    setup_landlock_tree(root);
+
+    auto allowed_dir = root / "allowed";
+    auto allowed_file = root / "allowed" / "a.txt";
+
+    ASSERT_EXIT(
+        {
+            unwrap_or_exit(set_no_new_privs());
+
+            auto lk = unwrap_or_exit(LandlockRuleSet::init());
+            unwrap_or_exit(lk.add_rule(allowed_dir.c_str(), LandlockAccess::Read));
+            unwrap_or_exit(lk.apply());
+
+            int fd = detail::sys_open(
+                allowed_file.c_str(),
+                O_WRONLY | O_CLOEXEC,
+                0
+            );
+
+            if (fd >= 0) {
+                detail::sys_close(fd);
+                std::exit(1);
+            }
+
+            std::exit(-fd);
+        },
+        ::testing::ExitedWithCode(EACCES),
+        ""
+    );
+
+    std::filesystem::remove_all(root);
+}
+
+TEST(LandlockBehavior, AllowsCreateFileWhenReadWriteIsGranted)
+{
+    auto root = make_test_root();
+    setup_landlock_tree(root);
+
+    auto allowed_dir = root / "allowed";
+    auto new_file = root / "allowed" / "created.txt";
+
+    ASSERT_EXIT(
+        {
+            unwrap_or_exit(set_no_new_privs());
+
+            auto lk = unwrap_or_exit(LandlockRuleSet::init());
+            unwrap_or_exit(lk.add_rule(allowed_dir.c_str(), LandlockAccess::ReadWrite));
+            unwrap_or_exit(lk.apply());
+
+            int fd = detail::sys_open(
+                new_file.c_str(),
+                O_CREAT | O_WRONLY | O_CLOEXEC,
+                0666
+            );
+
+            if (fd < 0) {
+                std::exit(-fd);
+            }
+
+            detail::sys_close(fd);
+            std::exit(0);
+        },
+        ::testing::ExitedWithCode(0),
+        ""
+    );
+
+    std::filesystem::remove_all(root);
+}
+
+TEST(LandlockBehavior, DeniesCreateFileWhenOnlyReadIsGranted)
+{
+    auto root = make_test_root();
+    setup_landlock_tree(root);
+
+    auto allowed_dir = root / "allowed";
+    auto new_file = root / "allowed" / "created.txt";
+
+    ASSERT_EXIT(
+        {
+            unwrap_or_exit(set_no_new_privs());
+
+            auto lk = unwrap_or_exit(LandlockRuleSet::init());
+            unwrap_or_exit(lk.add_rule(allowed_dir.c_str(), LandlockAccess::Read));
+            unwrap_or_exit(lk.apply());
+
+            int fd = detail::sys_open(
+                new_file.c_str(),
+                O_CREAT | O_WRONLY | O_CLOEXEC,
+                0666
+            );
+
+            if (fd >= 0) {
+                detail::sys_close(fd);
+                std::exit(1);
+            }
+
+            std::exit(-fd);
+        },
+        ::testing::ExitedWithCode(EACCES),
+        ""
+    );
+
+    std::filesystem::remove_all(root);
 }
--- code/library/cpp/tests/test_seccomp.cpp
+++ code/library/cpp/tests/test_seccomp.cpp
@@ -1,42 +1,245 @@
 #include <gtest/gtest.h>
+
 #include <mylib/seccomp.h>
 #include <mylib/no_new_privs.h>
 
-#include <sys/mman.h>
+#include <cerrno>
+#include <fcntl.h>
+#include <unistd.h>
 
 using namespace mylib;
 
-TEST(SeccompBuilder, Build) {
-	auto builder = SeccompBuilder::init();
-	EXPECT_TRUE(builder) << "init builder failed: " << builder.error();
+TEST(SeccompBuilder, InitSucceeds)
+{
+    auto builder = SeccompBuilder::init();
 
-	// adding new rules should work
-	EXPECT_TRUE(builder->allow("io")) << "allow rule failed";
-
-	// adding non-existing rules should NOT work
-	EXPECT_FALSE(builder->allow("jdijdsiojiodsj")) << "bad allow rule succeed";
-
-	// building the rule should work
-	auto rule = builder->build();
-	EXPECT_TRUE(rule) << "building failed";
+    ASSERT_TRUE(builder) << "init builder failed: " << builder.error();
 }
 
-TEST(SeccompBuilder, Apply) {
-	auto builder = SeccompBuilder::init();
-	EXPECT_TRUE(builder) << "init builder failed: " << builder.error();
+TEST(SeccompBuilder, InitNoDefaultsSucceeds)
+{
+    auto builder = SeccompBuilder::init_no_defaults();
 
-	// adding new rules should work
-	EXPECT_TRUE(builder->allow("io")) << "allow rule failed";
+    ASSERT_TRUE(builder) << "init_no_defaults builder failed: "
+                         << builder.error();
+}
 
-	// adding non-existing rules should NOT work
-	EXPECT_FALSE(builder->allow("jdijdsiojiodsj")) << "bad allow rule succeed";
+TEST(SeccompBuilder, AllowsExistingCategory)
+{
+    auto builder = SeccompBuilder::init();
 
-	// building the rule should work
-	auto rule = builder->build();
-	EXPECT_TRUE(rule) << "building failed";
+    ASSERT_TRUE(builder) << "init builder failed: " << builder.error();
 
-	set_no_new_privs();
+    auto res = builder->allow("io");
 
-	auto res = rule->view().apply();
-	EXPECT_TRUE(res) << "rule applying failed: " << res.error();
+    EXPECT_TRUE(res) << "allow(\"io\") failed: " << res.error();
+}
+
+TEST(SeccompBuilder, RejectsUnknownCategory)
+{
+    auto builder = SeccompBuilder::init();
+
+    ASSERT_TRUE(builder) << "init builder failed: " << builder.error();
+
+    auto res = builder->allow("definitely_not_a_real_category");
+
+    ASSERT_FALSE(res);
+    EXPECT_EQ(res.error(), ENOENT);
+}
+
+TEST(SeccompBuilder, BuildSucceedsAfterAddingCategories)
+{
+    auto builder = SeccompBuilder::init();
+
+    ASSERT_TRUE(builder) << "init builder failed: " << builder.error();
+
+    ASSERT_TRUE(builder->allow("io"));
+    ASSERT_TRUE(builder->allow("file_system"));
+
+    auto rule = builder->build();
+
+    EXPECT_TRUE(rule) << "building failed: " << rule.error();
+}
+
+TEST(SeccompBuilder, CannotAllowAfterBuild)
+{
+    auto builder = SeccompBuilder::init();
+
+    ASSERT_TRUE(builder) << "init builder failed: " << builder.error();
+
+    auto rule = builder->build();
+    ASSERT_TRUE(rule) << "building failed: " << rule.error();
+
+    auto res = builder->allow("io");
+
+    ASSERT_FALSE(res);
+    EXPECT_EQ(res.error(), EALREADY);
+}
+
+TEST(SeccompBuilder, BuildCanBeCalledMoreThanOnce)
+{
+    auto builder = SeccompBuilder::init();
+
+    ASSERT_TRUE(builder) << "init builder failed: " << builder.error();
+
+    auto rule1 = builder->build();
+    ASSERT_TRUE(rule1) << "first build failed: " << rule1.error();
+
+    auto rule2 = builder->build();
+    EXPECT_TRUE(rule2) << "second build failed: " << rule2.error();
+}
+
+TEST(SeccompBuilder, ApplyBasicIoFilterSucceeds)
+{
+    auto builder = SeccompBuilder::init();
+
+    ASSERT_TRUE(builder) << "init builder failed: " << builder.error();
+
+    ASSERT_TRUE(builder->allow("io"));
+
+    auto rule = builder->build();
+    ASSERT_TRUE(rule) << "building failed: " << rule.error();
+
+    auto nnp = set_no_new_privs();
+    ASSERT_TRUE(nnp) << "set_no_new_privs failed: " << nnp.error();
+
+    auto res = rule->view().apply();
+
+    EXPECT_TRUE(res) << "rule applying failed: " << res.error();
+}
+
+TEST(SeccompBuilderDeathTest, DisallowedSyscallKillsProcess)
+{
+    ASSERT_DEATH(
+        {
+            auto nnp = set_no_new_privs();
+            if (!nnp) {
+                std::exit(1);
+            }
+
+            auto builder = SeccompBuilder::init_no_defaults();
+            if (!builder) {
+                std::exit(1);
+            }
+
+            /*
+             * Csak a legszükségesebb kategóriákat engedjük.
+             * A file_system kategóriát szándékosan nem adjuk hozzá.
+             */
+            if (!builder->allow("io")) {
+                std::exit(1);
+            }
+
+            auto rule = builder->build();
+            if (!rule) {
+                std::exit(1);
+            }
+
+            auto applied = rule->view().apply();
+            if (!applied) {
+                std::exit(1);
+            }
+
+            /*
+             * Ha az openat nincs engedélyezve, akkor ennek a hívásnak
+             * a seccomp szűrő miatt meg kell ölnie a folyamatot.
+             */
+            int fd = open("/etc/passwd", O_RDONLY | O_CLOEXEC);
+
+            if (fd >= 0) {
+                close(fd);
+            }
+
+            std::exit(0);
+        },
+        ""
+    );
+}
+
+TEST(SeccompBuilderDeathTest, AllowedWriteDoesNotKillProcess)
+{
+    ASSERT_EXIT(
+        {
+            auto nnp = set_no_new_privs();
+            if (!nnp) {
+                std::exit(1);
+            }
+
+            auto builder = SeccompBuilder::init();
+            if (!builder) {
+                std::exit(1);
+            }
+
+            if (!builder->allow("io")) {
+                std::exit(1);
+            }
+
+            auto rule = builder->build();
+            if (!rule) {
+                std::exit(1);
+            }
+
+            auto applied = rule->view().apply();
+            if (!applied) {
+                std::exit(1);
+            }
+
+            char const msg[] = "seccomp write test\n";
+            ssize_t ret = write(STDOUT_FILENO, msg, sizeof(msg) - 1);
+
+            if (ret < 0) {
+                std::exit(1);
+            }
+
+            std::exit(0);
+        },
+        ::testing::ExitedWithCode(0),
+        ""
+    );
+}
+
+TEST(SeccompBuilderDeathTest, AllowedFileSystemOpenDoesNotKillProcess)
+{
+    ASSERT_EXIT(
+        {
+            auto nnp = set_no_new_privs();
+            if (!nnp) {
+                std::exit(1);
+            }
+
+            auto builder = SeccompBuilder::init();
+            if (!builder) {
+                std::exit(1);
+            }
+
+            if (!builder->allow("io")) {
+                std::exit(1);
+            }
+
+            if (!builder->allow("file_system")) {
+                std::exit(1);
+            }
+
+            auto rule = builder->build();
+            if (!rule) {
+                std::exit(1);
+            }
+
+            auto applied = rule->view().apply();
+            if (!applied) {
+                std::exit(1);
+            }
+
+            int fd = open("/etc/passwd", O_RDONLY | O_CLOEXEC);
+
+            if (fd < 0) {
+                std::exit(1);
+            }
+
+            close(fd);
+            std::exit(0);
+        },
+        ::testing::ExitedWithCode(0),
+        ""
+    );
 }
